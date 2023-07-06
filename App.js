a651931a1580bfe7025bfa7b39b4dafa76d234c7
Valerie <valerie.ielsch@gmail.com>
2023-07-06 10:20:57 +0200

    project init with apolo client

--- App.js
+++ App.js
@@ -1,7 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+} from "@apollo/client";
+import Constants from "expo-constants";
+const { manifest } = Constants;
 
 export default function App() {
+  const uri =
+    typeof manifest.packagerOpts === `object` && manifest.packagerOpts.dev
+      ? manifest.debuggerHost.split(`:`).shift().concat(`:4000/graphql`)
+      : `api.example.com`;
+  console.log(uri);
+  const client = new ApolloClient({
+    uri: `http://${uri}`,
+    cache: new InMemoryCache(),
+  });
+
+  client
+    .query({
+      query: gql`
+        query User {
+          users {
+            email
+            isAdmin
+          }
+        }
+      `,
+    })
+    .then((result) => console.log(result.data.users.map((user) => user.email)));
+
   return (
     <View style={styles.container}>
       <Text>Hello !</Text>
@@ -13,8 +44,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
